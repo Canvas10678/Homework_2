7f478773ca494bf05a5d3f3af1cd6f5061f6a329
Upsornsri Ketchum <78495462+Canvas10678@users.noreply.github.com>
2021-09-18 10:38:41 +0700

    Add files via upload

---
+++ Quiz2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+using System.Collections.Generic;
+namespace Test_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintRose();
+            PrintSunflower();
+        }
+        static void PrintRose()
+        {
+        
+        }
+        static void PrintSunflower()
+        {
+
+        }
+        class Rose
+        {
+            public int ID;
+            public string Plant;
+            public string Explean;
+            public int NumPlant;
+            public string HighPlant;
+            public string Circumference;
+            public Rose(int id,string plant, string explean, int numplnt, string highplant, string circumferences)
+            {
+                this.ID = id;
+                this.Plant = plant;
+                this.Explean = explean;
+                this.NumPlant = numplnt;
+                this.HighPlant = highplant;
+                this.Circumference = circumferences;
+            }
+        }
+        class Sunflower
+        {
+            public int ID;
+            public string Plant;
+            public string Explean;
+            public int NumPlant;
+            public string HighPlant;
+            public string Circumference;
+
+            public Sunflower(int id, string plant, string explean, int numplnt, string highplant, string circumferences)
+            {
+                this.ID = id;
+                this.Plant = plant;
+                this.Explean = explean;
+                this.NumPlant = numplnt;
+                this.HighPlant = highplant;
+                this.Circumference = circumferences;
+            }
+
+        }
+
+    }
+}
